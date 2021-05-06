78066d8fe2849aab1704e1d6acf1cd3c59e30c5b
José <jfmdev@gmail.com>
2021-05-06 13:32:07 -0300

    Minor Lint fixes

--- src/options.js
+++ src/options.js
@@ -1,9 +1,9 @@
-import App from './svelte/main.svelte';
+import App from "./svelte/main.svelte";
 
-console.log('Hello World!');
+console.log("Hello World!");
 
 const app = new App({
-    target: document.getElementById('root'), // entry point in ../public/index.html
+  target: document.getElementById("root"), // entry point in ../public/index.html
 });
 
 export default app;
