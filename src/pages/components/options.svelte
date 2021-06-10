9974d712d9d481b7613a41ed6cebb0e2170074cc
José <jfmdev@gmail.com>
2021-06-10 19:44:40 -0300

    Add 'Fork me on GitHub' ribbon to Options page

--- src/pages/components/options.svelte
+++ src/pages/components/options.svelte
@@ -55,3 +55,7 @@
     </div>
   </div>
 </div>
+
+<a href="https://github.com/jfmdev/chrome-extension-popurri">
+  <img loading="lazy" width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_darkblue_121621.png?resize=149%2C149" style="position:absolute; top:0; right:0;" alt="Fork me on GitHub" data-recalc-dims="1">
+</a>
